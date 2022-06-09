f204cc995aace35c3135833c4866bab093141e91
Astelun <johanchris12@gmail.com>
2022-06-09 08:19:43 +0200

    let SEO

--- index.php
+++ index.php
@@ -15,9 +15,16 @@
     <link rel="icon" href="images/blad_logo.png">
 
     <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
-    <meta name="robots" content="All">
-    <meta name="author" content="Udgiver">
-    <meta name="copyright" content="Information om copyright">
+    <meta name="robots" content="All" />
+    <meta name="author" content="Udgiver" />
+    <meta name="copyright" content="Information om copyright" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Indexside med oversigt over produktkategorier samt fængende billeder." />
+    <meta name="keywords" content="bestilling, blomsterbutik, service"/>
+    <meta name="robots" content="index,follow" />
+    <meta property="og:title" content="Botanica Blomster" />
+    <meta property="og:image" content="" />
+    <meta property="og:description" content="Botanica er en blomsterbutik, der tilbyder de fineste blomster, i samarbejde med top-klasse service og et fællesskab om at holde af blomster. Botanicas webshop giver også muligheden for at bestille dine blomster hjem online." />
 
     <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
     <link href="css/Bootstrap.css" rel="stylesheet" type="text/css">
@@ -44,18 +51,18 @@
     <div class="row bodybg2">
         <div class="col-lg-6 text-center my-auto">
             <div class="navbar-brand border-0 col-lg-7 mx-auto" style="background-color: #F7F4F1; border-radius: 35px">
-                <img src="images/BLogo.png" class="logo col-10">
+                <img src="images/BLogo.png" class="logo col-10" alt="Botanica">
             </div>
 
             <a href="#Nede">
                 <button class="navbar-brand border-0 col-lg-1 bg-transparent py-5">
-                    <img src="images/downer_alt.png" class="pil kat col-12">
+                    <img src="images/downer_alt.png" class="pil kat col-12" alt="Pil ned">
                 </button>
             </a>
 
         </div>
         <div class="col-6 m-0 p-0 toppen">
-            <img src="images/annie-splash.png" class="img-fluid vh-100 float-lg-end">
+            <img src="images/annie-splash.png" alt="Blomster billede" class="img-fluid vh-100 float-lg-end">
         </div>
     </div>
 </div>
@@ -63,13 +70,13 @@
 
 
 <div class="m-0 p-0 d-flex flex-row-reverse">
-    <img src="images/dum_plante.png" class="bagved m-0 p-0" style="position: fixed">
+    <img src="images/dum_plante.png" alt="Blomster billede" class="bagved m-0 p-0" style="position: fixed">
 </div>
 
 <div class="big">
     <?php include "includes/navigation.html"; ?>
     <button class="navbar-brand border-0 col-12 bg-transparent tiny position-absolute bottom-0">
-        <a href="#mini"><img src="images/downer_alt.png" class="pil kat col-2"></a>
+        <a href="#mini"><img src="images/downer_alt.png" class="pil kat col-2" alt="Pil ned"></a>
     </button>
 </div>
 <div class="container-fluid py-5 col-8 col-lg-12" style="font-family: 'Playfair Display'" id="mini">
@@ -79,13 +86,13 @@
         <div class="card col-10 col-lg-3 p-0 mx-auto my-5 my-lg-0">
             <button class="card-body kat bodybg1 border-0">
                 <h3 class="card-title text-center">Buketter</h3>
-                <img src="images/IC_buket.jpg" class="card-img-top">
+                <img src="images/IC_buket.jpg" class="card-img-top" alt="Blomster buket">
             </button>
         </div>
         <div class="card col-10 col-lg-3 p-0 mx-auto mt-5 my-lg-0">
             <button class="card-body kat bodybg3 border-0">
                 <h3 class="card-title text-center">Bryllup</h3>
-                <img src="images/DF_bryl.jpg" class="card-img-top">
+                <img src="images/DF_bryl.jpg" class="card-img-top" alt="Brude par">
             </button>
         </div>
     </div>
@@ -94,19 +101,19 @@
         <div class="card col-10 col-lg-3 p-0 mx-auto my-5 my-lg-0">
             <button class="card-body kat bodybg3 border-0">
                 <h3 class="card-title text-center">Begravelse</h3>
-                <img src="images/RL_grav.jpg" class="card-img-top">
+                <img src="images/RL_grav.jpg" class="card-img-top" alt="Begravelse">
             </button>
         </div>
         <div class="card col-10 col-lg-3 p-0 mx-auto my-5 my-lg-0">
             <button class="card-body kat bodybg1 border-0">
                 <h3 class="card-title text-center">Anledning</h3>
-                <img src="images/AS_valen.jpg" class="card-img-top">
+                <img src="images/AS_valen.jpg" class="card-img-top" alt="Blomster hjerte">
             </button>
         </div>
         <div class="card col-10 col-lg-3 p-0 mx-auto my-5 my-lg-0">
             <button class="card-body kat bodybg3 border-0">
                 <h3 class="card-title text-center">Tilbehør</h3>
-                <img src="images/EK_choco.jpg" class="card-img-top">
+                <img src="images/EK_choco.jpg" class="card-img-top" alt="Chokolader">
             </button>
         </div>
     </div>
@@ -125,22 +132,22 @@
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="5000">
-                <img src="images/annie-splash.png" class="d-block w-75 mx-auto" alt="...">
+                <img src="images/annie-splash.png" class="d-block w-75 mx-auto" alt="Plante">
             </div>
             <div class="carousel-item" data-bs-interval="5000">
-                <img src="images/hero_varer.jpg" class="d-block w-75 mx-auto" alt="...">
+                <img src="images/hero_varer.jpg" class="d-block w-75 mx-auto" alt="Butik">
             </div>
             <div class="carousel-item" data-bs-interval="5000">
-                <img src="images/IC_buket.jpg" class="d-block w-75 mx-auto" alt="...">
+                <img src="images/IC_buket.jpg" class="d-block w-75 mx-auto" alt="Blomsterbuket">
             </div>
             <div class="carousel-item" data-bs-interval="5000">
-                <img src="images/AS_valen.jpg" class="d-block w-75 mx-auto" alt="...">
+                <img src="images/AS_valen.jpg" class="d-block w-75 mx-auto" alt="Blomster hjerte">
             </div>
             <div class="carousel-item" data-bs-interval="5000">
-                <img src="images/DF_bryl.jpg" class="d-block w-75 mx-auto" alt="...">
+                <img src="images/DF_bryl.jpg" class="d-block w-75 mx-auto" alt="Brudepar">
             </div>
             <div class="carousel-item" data-bs-interval="5000">
-                <img src="images/EK_choco.jpg" class="d-block w-75 mx-auto" alt="...">
+                <img src="images/EK_choco.jpg" class="d-block w-75 mx-auto" alt="Chokolade">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
